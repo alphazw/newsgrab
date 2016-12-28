cdbdd9c28339f56ab5883dbb6ce98d688af8be58
alphazw <wei.zhang@ninestar.local>
2016-12-28 11:47:23 +0800

    iRecycler
    Test passed on iRecycler

--- newsgrab.py
+++ newsgrab.py
@@ -5,6 +5,8 @@
 # + New
 
 from openpyxl import load_workbook
+from bs4 import BeautifulSoup
+import requests, re
 import datetime
 RuleFN = 'rulelist.xlsx'
 
@@ -27,3 +29,34 @@ def get_rulelist(FN):
         RW = []
 
     return RL
+
+def get_indexpage(url):
+    header = {'Accept-Language':'zh-CN,zh;q=0.8','User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36','Connection':'close','Referer': 'https://www.baidu.com/'}
+    indexpage =  requests.get(url,headers=header)
+    return indexpage.content
+
+#get a newslist from the listpage by applying the rule_url
+def get_newslist(indexpage_url, rule_url):
+    indexcontent = get_indexpage(indexpage_url)
+    indexpage = BeautifulSoup(indexcontent,'lxml')
+    p= indexpage.select(rule_url)
+    # p = re.compile(rule_url, re.S)
+    #alllist = re.findall(p, indexpage)
+
+    return p
+
+def trans2list(par):
+    a = []
+    for b in par:
+        a.append(b['href'])
+
+    return a
+
+def test_irecycler():
+    url = r"http://www.irecyclingtimes.com/News-list?news_column_id=16_8_9_11_5_17_7_12"
+    rule_url = r"table#news_list_tb a"
+
+    print trans2list(get_newslist(url,rule_url))
+
+if __name__ == "__main__":
+    test_irecycler()
