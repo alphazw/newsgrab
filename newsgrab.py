e2c594f296c9a8dbfed7741d64ac620221ee422f
alphazw <wei.zhang@ninestar.local>
2016-12-29 09:01:32 +0800

    2016-12-29

--- newsgrab.py
+++ newsgrab.py
@@ -1,15 +1,8 @@
-#
-# Update:
-#
-# 2016-12-20
-# + New
-
 from openpyxl import load_workbook
 from bs4 import BeautifulSoup
 import requests, re
 import datetime
 RuleFN = 'rulelist.xlsx'
-
 
 #Build file name with today's date
 def get_todayFN():
@@ -39,16 +32,30 @@ def get_indexpage(url):
 def get_newslist(indexpage_url, rule_url):
     indexcontent = get_indexpage(indexpage_url)
     indexpage = BeautifulSoup(indexcontent,'lxml')
-    p= indexpage.select(rule_url)
+    result= indexpage.select(rule_url)
+    return result
 
-    return p
-
+#get href attribution from the given result in a list for <a href=...></a>
 def get_href(par):
     tmp_a = []
     for b in par:
         tmp_a.append(b['href'])
 
     return tmp_a
+
+#Complete url, if the url not starts with domain name, if the base not ends with /, than plus /
+#if the url starts with /, than remove /
+def completeurl(base, url):
+
+    if not base.endswith('/'): base = base+'/'
+
+    if not url.startswith('base'):
+        if url.startswith('/'):
+            return base+url[1:]
+        else:
+            return base+url
+    else:
+        return url
 
 def test_irecycler():
     url = r"http://www.irecyclingtimes.com/News-list?news_column_id=16_8_9_11_5_17_7_12"
@@ -65,3 +72,15 @@ def test_actionintell():
 if __name__ == "__main__":
     # test_irecycler()
     # test_actionintell()
+    url =[]
+    # url.append([r'http://www.irecyclingtimes.com/News-list?news_column_id=16_8_9_11_5_17_7_12',r'table#news_list_tb a'])
+    # url.append([r'http://www.action-intell.com/category/news-briefing/','h1.entry-title a'])
+    # url.append([r'http://www.theimagingchannel.com/channel-news','h3 a'])
+    # url.append([r'http://www.therecycler.com/latest-news/','h2 a'])
+    # url.append(['http://tonernews.com/forums/forum/industry-news/','li.bbp-body ul li.bbp-topic-title a.'])
+    url.append(['http://www.rechargeasia.com/index.php/Table/News/3D-Printing/','ul.list_ulList_1 li a'])
+    print url
+    a=[]
+    for u in url:
+        a =a + get_href(get_newslist(u[0],u[1]))
+    print a
