f9d3cdcf213485fc649580b5a1ff5ecf40df914d
alphazw <alphazw@163.com>
2017-01-10 00:20:58 +0800

    2017-01-09
    retrievenewlist, retrievenewscontent, complete db structure

--- newsgrab.py
+++ newsgrab.py
@@ -1,38 +1,18 @@
 from openpyxl import load_workbook
 from bs4 import BeautifulSoup
 import requests, re, sqlite3, hashlib
-import datetime
+import datetime, smtplib, email
 
-RuleFN = 'rulelist.xlsx'
-RuleDB = 'data.db'
+DBFile = '/Users/Mavericks/Documents/newsgrab/data.db'
 DEBUG = True
 
 def log(s):
     if DEBUG:
         print s
 
-#Build file name with today's date
-def get_todayFN():
-    return datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
-
 #generate hash
 def generate_hash(text=""):
     return hashlib.sha256(text).hexdigest()
-
-#get the rule list from a given excel workbook, return the value of the value into a dict
-def get_rulelist(FN):
-    wb = load_workbook(filename=FN, read_only=True)
-    ws = wb['Sheet1']
-    (RL,RW) = ([],[])
-
-    for row in ws.rows:
-        for cell in row:
-            RW.append(cell.value)
-
-        RL.append(RW)
-        RW = []
-
-    return RL
 
 def get_indexpage(url):
     header = {'Accept-Language':'zh-CN,zh;q=0.8','User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36','Connection':'close','Referer': 'https://www.baidu.com/'}
@@ -81,14 +61,15 @@ def completeurl(base, url):
 
 def retrieveNewlist():
     try:
-        conn = sqlite3.connect('data.db')
+        conn = sqlite3.connect(DBFile)
         cur = conn.cursor()
-        sql_sitelist = 'SELECT name,baseurl,listpageurl, listpageurl_rule, sitelist_id, articletiel_rule, articlecontent_rule, articleclick_rule, articledate_rule FROM sitelist'
+        sql_sitelist = 'SELECT name, baseurl,listpageurl, listpageurl_rule, sitelist_id, articletitle_rule, articlecontent_rule, articleclick_rule, articledate_rule FROM sitelist'
+        # sql_sitelist = "select * from sitelist"
         cur.execute(sql_sitelist)
         sitelist = cur.fetchall()
 
         for u in sitelist:
-            u_name, u_baseurl, u_listpageurl, u_listpageurl_rule, u_sitelist_id, u_articletitle_rule, u_articlecontent_rule, u_articleclicks_rule, u_articledate_rule = u[0], u[1], u[2], u[3], u[4], u[5], u[6], u[7], u[8]
+            u_name, u_baseurl, u_listpageurl, u_listpageurl_rule, u_sitelist_id, u_articletitle_rule, u_articlecontent_rule, u_articleclick_rule, u_articledate_rule = u[0], u[1], u[2], u[3], u[4], u[5], u[6], u[7], u[8]
             url_newslist = get_href(get_newslist(u_listpageurl,u_listpageurl_rule))
             # log("_"*20)
             # log('Retrieving NewsList from ')
@@ -103,8 +84,8 @@ def retrieveNewlist():
                 u_hash = generate_hash(u_n)
 
                 if not duplicatecheck(u_hash):
-                    sql_newslist = "INSERT INTO newslist (newsurl, sitelist_id, hash, adddate, articletiel_rule, articlecontent_rule, articleclick_rule, articledate_rule ) VALUES (%s,%s,%s,%s.%s,%s,%s)"
-                    conn.execute(sql_newslist,(u_articletitle_rule, u_articlecontent_rule, u_articleclicks_rule, u_articledate_rule))
+                    sql_newslist = "INSERT INTO newslist (newsurl, sitelist_id, hash, adddate, articletitle_rule, articlecontent_rule, articleclick_rule, articledate_rule ) VALUES (%s,%s,%s,%s.%s,%s,%s)"
+                    conn.execute(sql_newslist,(u_articletitle_rule, u_articlecontent_rule, u_articleclick_rule, u_articledate_rule))
                 # log(u_n)
                 # log(generate_hash(u_n))
 
@@ -135,24 +116,24 @@ def retrieveNewlist():
     return True
 
 #to-do:needs get rule of each urls.
-def retrieveNews():
+def retrieveNewsContent():
     try:
-        conn = sqlite3.connect('data.db')
+        conn = sqlite3.connect(DBFile)
         cur = conn.cursor()
-        sitelistsql = 'select * from newslist WHERE date=%s'+ datetime.datetime.strftime('Y-M-D')
-        # sitelistsql = 'select * from sitelist where enable <> false'
+        sitelistsql = 'SELECT * from newslist WHERE date=%s'+ datetime.datetime.strftime('Y-M-D')
+        
         cur.execute(sitelistsql)
         sitelist = cur.fetchall()
 
         for u in sitelist:
-            news_title = ""
-            news_content = ""
-            news_clicks = ""
-            news_date = ""
+            
+            u_newsurl, u_articletitle_rule, u_articlecontent_rule, u_articleclick_rule, u_articledate_rule = u[1], u[2], u[3], u[4], u[5]
+            news_title, news_content, news_click, news_date = get_article(u_newsurl,u_articletitle_rule, u_articlecontent_rule, u_articleclick_rule, u_articledate_rule)
+
             adddate = datetime.datetime.strftime('Y-M-D')
             u_hash = generate_hash(u)
-            insertsql = "INSERT INTO content (title, content, clicks, date, adddate, hash) VALUES(%s, %s, %s, %s, %s, %s )"
-            cur.execute(insertsql,(news_title, news_content, news_clicks, news_date, adddate, u_hash))
+            insertsql = "INSERT INTO newscontent (title, content, click, articledate, adddate, hash) VALUES(%s, %s, %s, %s, %s, %s )"
+            cur.execute(insertsql,(news_title, news_content, news_click, news_articledate, adddate, u_hash))
 
 
     except sqlite3.Error as e:
@@ -173,12 +154,12 @@ def duplicatecheck(hash):
     else:
         return False
 
-def get_newscontent(newsurl, at_rule, ac_rule, ad_rule, ack_rule):
-    sql_newscontent = "INSERT INTO newscontent (articletitle, articlecontent,  articleclicks, articledate, hash, adddate) VALUES(%s, %s, %s, %s, %s, %s)"
-    newscontent = get_article(newsurl, at_rule,ac_rule, ack_rule, ad_rule)
-    hash, adddate = generate_hash(newsurl), datetime.datetime.now().date()
+# def get_newscontent(newsurl, at_rule, ac_rule, ad_rule, ack_rule):
+#     sql_newscontent = "INSERT INTO newscontent (articletitle, articlecontent,  articleclick, articledate, hash, adddate) VALUES(%s, %s, %s, %s, %s, %s)"
+#     newscontent = get_article(newsurl, at_rule,ac_rule, ack_rule, ad_rule)
+#     hash, adddate = generate_hash(newsurl), datetime.datetime.now().date()
 
-    conn.executive(sql_newscontent,(newscontent, hash, adddate))
+#     conn.executive(sql_newscontent,(newscontent, hash, adddate))
 
 
 #build reports from the content in the database by selecting by today()
@@ -191,28 +172,28 @@ def buildReport():
 
     for cl in contentlist:
         cl_articletitle, cl_articlecontent, cl_articleclick, cl_articledate = cl[0],cl[1],cl[2],cl[3],c[4]
+        cl_brief = generate_brief(cl_articlecontent)
 
-        log(cl_articleclick)
-        log(cl_articlecontent)
-        log(cl_articleclick)
+        log("-"*20)
+        log(cl_articletitle)
         log(cl_articledate)
+        log(cl_brief)
+        log("="*10)
+        log(cl_articlecontent)
 
     return True
 
-def sendemail(emaillist, content):
-    return True
+# sendemail(), used for sending email out using SMTP protocol
+def sendemail(username, password, replyfrom, replyto, emailbody):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.starttls()
+        server.login(username, password)
+        server.sendmail(replyfrom, replyto, emailbody)
+        server.quit()
+    except:
+        show_status('unable to reply email to %s' % replyto)
 
-def test_irecycler():
-    url = r"http://www.irecyclingtimes.com/News-list?news_column_id=16_8_9_11_5_17_7_12"
-    rule_url = r"table#news_list_tb a"
-
-    print get_href(get_newslist(url,rule_url))
-
-def test_actionintell():
-    url = "http://www.action-intell.com/category/news-briefing/"
-    rule_url="h1.entry-title a"
-
-    print get_href(get_newslist(url, rule_url))
 
 if __name__ == "__main__":
     # test_irecycler()
